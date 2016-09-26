461ecdf0da95a7a6f3e56ec9213cc761bc141027
Bo Granlund <bgranlun@cs.helsinki.fi>
2016-09-26 19:28:36 +0300

    New layout

--- config/routes.php
+++ config/routes.php
@@ -1,12 +1,4 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-
-  //$routes->get('/tietokantayhteys', function () {
-	//print "AKALAAA";
-	//DB::test_connection();
- // });
-
   $routes->get('/', function() {
     PizzaController::index();
   });
@@ -19,61 +11,47 @@ use \Psr\Http\Message\ResponseInterface as Response;
         DB::test_connection();
   });
 
-	//$routes->get('/hello/{msg}', function (Request $request, Response $response, $args) {
+//$routes->get('/koosta', function () use ($routes) {
+//	tPizzaController::koosta();
+//});
+//$routes->post('/op_uusipizza', function () use ($routes) {
+//	tPizzaController::op_uusipizza($routes);
+//});
+//$routes->get('/uusilisuke', function () use ($routes) {
+//	tPizzaController::uusilisuke($routes);
+//});
+//$routes->post('/op_uusilisuke', function () use ($routes) {
+//        tPizzaController::op_uusilisuke($routes);
+//});
+//$routes->get('/uusmenu', function () use ($routes) {
+//	PizzaController::pizzas();
+//});
 
-	$routes->get('/hello/kala', function () use ($routes) {
-		//print "KLFLLFLFL : " . $id;
-		$routes->redirect("/index.php");
-		//print "haukikala";
-    		//$name = $request->getAttribute('name');
-    		//$response->getBody()->write("Hello, $name");
 
-   		 //return $response;
-	});
-
-
-$routes->get('/kkamyja/:kalavar', function () use ($routes) {
-	$kalavar = $routes->request->get('kalavar');
-	print $kalavar . " saatoa ";
-    echo "Hello, " . $kalavar;
+/*
+ * Pizzoja
+ */
+$routes->get('/menu', function() use ($routes) {
+	PizzaController::pizzas();
 });
-$routes->post('/haukikala/new', function() use ($routes) {
-	echo "Hello world<br><br>";
-	$kala = $routes->request->post('kalavar');
-	print "jou: " . $kala;
+$routes->get('/menu/add_templ', function() {
+	PizzaController::add_templ();
 });
-
-$routes->get('/ticket/{id}', function (Request $request, Response $response, $args) {
-    $ticket_id = (int)$args['id'];
-	print "kaala: " . $ticket_id . "<br>";
+$routes->post('/menu/add', function() use ($routes) {
+	PizzaController::add($routes);
 });
-
-$routes->post('/ticket/new', function (Request $request, Response $response) {
-	print "tama antaa uuden postin settiin ";
-
-});
-
-$routes->post('/hamyja', function (Request $request, Response $response) {
-	print "HAMYJA";
+$routes->get('/menu/:numero', function($numero) use ($routes) {
+	PizzaController::get_id($numero, $routes);
 });
 
-$routes->get('/menu', function () use ($routes) {
-	PizzaController::menu();
+/*
+ * Lisukkeita
+ */
+$routes->get('/lisuke/add_templ', function () {
+	LisukeController::add_templ();
 });
-$routes->get('/koosta', function () use ($routes) {
-	PizzaController::koosta();
-});
-$routes->post('/op_uusipizza', function () use ($routes) {
-	PizzaController::op_uusipizza($routes);
-});
-$routes->get('/uusilisuke', function () use ($routes) {
-	PizzaController::uusilisuke($routes);
-});
-$routes->post('/op_uusilisuke', function () use ($routes) {
-        PizzaController::op_uusilisuke($routes);
+$routes->post('/lisuke/add', function() use ($routes) {
+	LisukeController::add($routes);
 });
 
-
-
-//print "HJAIKDDLDLDL";
 ?>
